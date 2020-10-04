6ce85ea4cb84d3e6cb9576077eab62989d01a9b3
Pichandi <apichu@gmail.com>
2020-10-04 20:01:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Welcome pichandi.
-        </p>
-         
-          done with docker jein kin 
-        </a>
-      </header>
+      <h1>Hi Pichandi Anchineyan</h1>
     </div>
   );
 }
