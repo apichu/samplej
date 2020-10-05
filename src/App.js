fa4cced81870c7cbf617015023a73dcb39708681
Pichandi Anchineyan <apichu@gmail.com>
2020-10-05 08:27:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+ 
       <h1>Hi Pichandi Anchineyan welocime</h1>
-=======
-      <h1>Hi xxdsdsajhhdsPichandi Anchineyan</h1>
->>>>>>> 2422d6b93760526a651f90251d1a5658e5eef03d
+ 
+   
+ 
     </div>
   );
 }
