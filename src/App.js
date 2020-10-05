e4d8c62bf32823a83eaee1e4220fa0906d90be8c
Pichandi <apichu@gmail.com>
2020-10-05 10:36:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hi Pichandid Anchineyan Welcome back</h1>
+      <h1>Hi Pichandid Anchineyan Welcome HCG</h1>
     </div>
   );
 }
